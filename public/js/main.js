e25f68d43ed05527365df9437ede930c7e563453
okey E <okeysmooed@gmail.com>
2024-05-26 22:14:59 +0000

    added form

--- public/js/main.js
+++ public/js/main.js
@@ -67,3 +67,17 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+// --------------------------Sign-in Form------------------------------------//
+const container = document.getElementById('container');
+const overlayCon = document.getElementById('overlayCon');
+const overlayBtn = document.getElementById('overlayBtn');
+
+overlayBtn.addEventListener('click', ()=> {
+    container.classList.toggle('right-panel-active');
+
+    overlayBtn.classList.remove('btnScaled');
+    window.requestAnimationFrame( () => {
+        overlayBtn.classList.add('btnScaled');
+    })
+} )
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const connectDB = require('./config/database')
+// const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
-require('dotenv').config({path: './config/.env'})
+// require('dotenv').config({path: './config/.env'})
 
-connectDB()
+// connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -15,7 +15,9 @@ app.use(express.json())
 
 app.use('/', homeRoutes)
 app.use('/todos', todoRoutes)
+
+const PORT = process.env.PORT || 3000; // Use the port from the environment variables or default to 3000
  
-app.listen(process.env.PORT, ()=>{
+app.listen(PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
