d4692c44b74b4b013f82069b6eea7fb8713ee708
okey E <okeysmooed@gmail.com>
2024-06-04 20:57:25 +0000

    added routers

--- routes/recipe.js
+++ routes/recipe.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipe');
 
- router.get('/', recipeController.getRandomRecipes); // Route to fetch random recipes
+ router.get('/', ensureAuth, recipeController.getRandomRecipes); // Route to fetch random recipes
 router.get('/view', recipeController.viewRecipes); // Route to render the recipe page
 
 module.exports = router;
