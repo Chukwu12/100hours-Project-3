f6a70ba61ef6d09859b95ecda497bfbbc33aba06
okey E <okeysmooed@gmail.com>
2024-06-04 17:31:24 +0000

    upload new files

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
 const express = require('express')
 const app = express()
-// const connectDB = require('./config/database')
-const homeRoutes = require('./routes/home')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
 const todoRoutes = require('./routes/todos')
-const recipeRoutes = require('./routes/recipe') // Corrected path to the recipe route
 
- require('dotenv').config({path: './config/.env'})
+require('dotenv').config({path: './config/.env'})
 
-// connectDB()
+// Passport config
+require('./config/passport')(passport)
+
+connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+  
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use('/', homeRoutes)
+app.use(flash())
+  
+app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
-app.use('/recipe', recipeRoutes) // Use the correct route for recipes
-
-
-
-const PORT = process.env.PORT || 3000; // Use the port from the environment variables or default to 3000
  
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
