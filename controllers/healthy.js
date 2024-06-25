156bea000c539639b22882fa67869b5b714df012
Okechukwu Egeruoh <oegeruoh@gmail.com>
2024-06-25 13:55:03 -0400

    update

--- controllers/healthy.js
+++ controllers/healthy.js
@@ -1,7 +1,7 @@
 // controllers/healthy.js
 const axios = require('axios');
 
-const RECIPES_API_KEY = process.env.RECIPES_API_KEY ||'15b2edef64f24d2c95b3cc72e3ad8f87';
+const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
 const HEALTHY_RECIPES_API_URL = 'https://api.spoonacular.com/recipes/random?apiKey=15b2edef64f24d2c95b3cc72e3ad8f87&diet=vegetarian';
 
 
