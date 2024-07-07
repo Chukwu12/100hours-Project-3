b84fbb6748986748d5dd9bdc7c71f5b7f15e9225
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-07-07 20:12:03 +0000

    update

--- routes/health.js
+++ routes/health.js
@@ -7,6 +7,7 @@ const { getHealthRecipes, viewHealthRecipes } = require('../controllers/healthy'
 // Route to get random vegetarian recipes
  router.get('/',getHealthRecipes); // Route to fetch random recipes
  // Route to view the recipes
-router.get('/view',viewHealthRecipes); // Route to render the recipe page
+router.get('/view',
+    viewHealthRecipes); // Route to render the recipe page
 
 module.exports = router;
