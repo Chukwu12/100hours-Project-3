5700442992d3cbd38cf2b99312aec5248fa36e45
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-07-12 21:02:02 +0000

    updated navbar

--- controllers/cuisine.js
+++ controllers/cuisine.js
@@ -1,52 +1,34 @@
 // controllers/cuisine.js
 const axios = require('axios');
-// const Recipe = require('../models/Recipe');
 
-const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
-const CUSINE_API_URL = 'https://api.spoonacular.com/recipes/complexSearch';
-
-
-// Function to get recipes by cuisine type
 const getCuisineRecipes = async (req, res) => {
-    const { type } = req.params; // 'african', 'american', 'asian', 'mexican', etc.
-    
- 
-    try 
-    {
-        if (!RECIPES_API_KEY) {
-            return res.status(401).json({ message: 'API key is missing' });
-        }
+    const type = req.params.type;
+    const apiKey = process.env.RECIPES_API_KEY;
 
-        const response = await axios.get(CUSINE_API_URL, {
+    if (!apiKey) {
+        return res.status(500).send('API Key is not defined');
+    }
+
+    try {
+        const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch`, {
             params: {
-                apiKey: RECIPES_API_KEY,
-                number: 8,
                 cuisine: type,
-                limitLicense: true,
+                apiKey: apiKey
             }
         });
-        console.log(response.data.results);
-
-
-             // Render the corresponding EJS template (e.g., 'african.ejs', 'american.ejs')
-             res.render('cusine', { recipes: response.data.results, cuisineType: type });
+        const recipes = response.data.results;
+        res.render('cuisine', { recipes, type });
     } catch (error) {
-        console.error('Error fetching data from Spoonacular:', error.message);
-        res.status(500).send('Server Error');
+        res.status(500).send('Error fetching recipes');
     }
 };
 
+const viewCuisineRecipes = (req, res) => {
+    // Logic to view all cuisine recipes
+    res.send('Viewing all cuisine recipes');
+};
 
-
-
-
-// Function to view all cuisine recipes
-
-
-module.exports = { getCuisineRecipes };
-
-
-
-
-
-
+module.exports = {
+    getCuisineRecipes,
+    viewCuisineRecipes
+};
