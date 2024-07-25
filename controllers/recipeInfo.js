233500350ea0bcf7564146ece1182c5d361beda3
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-07-25 15:51:11 +0000

    updated work

--- controllers/recipeInfo.js
+++ controllers/recipeInfo.js
@@ -1,4 +1,4 @@
 // controllers/recipeInfo.js
 const axios = require('axios');
 
-const RECIPES_API_KEY = process.env.RECIPES_API_KEY || '15b2edef64f24d2c95b3cc72e3ad8f87';
+const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
