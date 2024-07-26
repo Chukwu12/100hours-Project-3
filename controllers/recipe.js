bf58fed61919b770c68e0c386f0debaa5c39a694
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-07-26 00:12:18 +0000

    updated cusines

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,7 +1,7 @@
 // controllers/recipe.js
 const axios = require('axios');
 
-const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
+const RECIPES_API_KEY = process.env.RECIPES_API_KEY || '15b2edef64f24d2c95b3cc72e3ad8f87';
 const RECIPES_API_URL = 'https://api.spoonacular.com/recipes/random';
 const RECIPE_DETAILS_API_URL = 'https://api.spoonacular.com/recipes/{id}/information';
 
