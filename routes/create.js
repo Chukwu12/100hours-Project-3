c0afbf776f2ef55ffb566fcce30f4ec8d67ecd55
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2024-10-15 17:15:26 +0000

    update

--- routes/create.js
+++ routes/create.js
@@ -8,6 +8,9 @@ router.get('/createRecipes', (req, res) => {
     res.render('createRecipes'); // Ensure this points to the correct EJS file
 });
 
+// Route to rending food triva
+router.get('/createRecipes', createController.foodFacts);
+
 router.get('/createRecipes', createController.createRecipe)
  //Enables user to create post w/ cloudinary for media uploads
 // router.post("/createRecipes", upload.single("file"), createController.createRecipe);
