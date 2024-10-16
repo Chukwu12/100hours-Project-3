e6e918f875656bf9868b34ef484fedd321556a73
okey E <okeysmooed@gmail.com>
2024-10-16 13:57:17 +0000

    update

--- controllers/create.js
+++ controllers/create.js
@@ -1,33 +1,33 @@
-const API_FOOD_TRIVA = 'https://api.spoonacular.com/food/trivia/random';
-const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
+// const API_FOOD_TRIVA = 'https://api.spoonacular.com/food/trivia/random';
+//  const RECIPES_API_KEY = process.env.RECIPES_API_KEY;
 
-const foodFacts = async (req, res) => {
-    try {
-        if (!RECIPES_API_KEY) {
-            throw new Error('API key is missing');
-        }
+// const foodFacts = async (req, res) => {
+//     try {
+//         if (!RECIPES_API_KEY) {
+//             throw new Error('API key is missing');
+//         }
 
-        const response = await axios.get(API_FOOD_TRIVA, {
-            params: {
-                apiKey: RECIPES_API_KEY,
+//         const response = await axios.get(API_FOOD_TRIVA, {
+//             params: {
+//                 apiKey: RECIPES_API_KEY,
     
-            }
-        });
+//             }
+//         });
 
-         // Check for a valid response
-         if (!response || !response.data) {
-            return res.status(500).json({ message: 'Invalid API response' });
-        }
+//          // Check for a valid response
+//          if (!response || !response.data) {
+//             return res.status(500).json({ message: 'Invalid API response' });
+//         }
 
-        const trivia = response.data.text;
-         // Render or return the trivia as needed
-         res.render('createRecipes', { trivia });
+//         const trivia = response.data.text;
+//          // Render or return the trivia as needed
+//          res.render('createRecipes', { trivia });
         
-        } catch (error) {
-            console.error('Error fetching food trivia:', error);
-            res.status(500).json({ message: 'An error occurred while fetching food trivia.' });
-        }
-    };
+//         } catch (error) {
+//             console.error('Error fetching food trivia:', error);
+//             res.status(500).json({ message: 'An error occurred while fetching food trivia.' });
+//         }
+//     };
 
 
 const createRecipe = async (req, res) => {
@@ -75,5 +75,5 @@ const getCreateRecipePage = (req, res) => {
 module.exports = {
     createRecipe,
     getCreateRecipePage,
-    foodFacts,
+    // foodFacts,
   }
