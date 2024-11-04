6276e81b385f8fa3cd1dab82b436f32f59159f6b
okey E <okeysmooed@gmail.com>
2024-11-04 17:17:47 +0000

    updated login responsive features

--- public/js/main.js
+++ public/js/main.js
@@ -75,15 +75,28 @@ document.addEventListener('DOMContentLoaded', () => {
 const sign_in_btn = document.querySelector('#sign-in-btn');
 const sign_up_btn = document.querySelector('#sign-up-btn');
 const container = document.querySelector('.container');
+const sign_in_btn2 = document.querySelector('#sign-in-btn2');
+const sign_up_btn2 = document.querySelector('#sign-up-btn2');
 
+ // Check if all required elements are present
 if (sign_up_btn && sign_in_btn && container) {
 sign_up_btn.addEventListener('click',() =>{
     container.classList.add('sign-up-mode');
 });
 
+  // Add event listener for sign-in button
 sign_in_btn.addEventListener('click', ()=> {
     container.classList.remove('sign-up-mode');
 });
+
+sign_up_btn2.addEventListener('click',() =>{
+    container.classList.add('sign-up-mode2');
+});
+
+sign_in_btn2.addEventListener('click', ()=> {
+    container.classList.remove('sign-up-mode2');
+});
+
 } else {
     console.error('Required elements are not found in the DOM');
 }
