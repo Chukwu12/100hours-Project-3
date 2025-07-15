fcf55b77a0be56261f8a7bf473b3abebcbf65609
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2025-07-15 21:20:29 +0000

    update

--- config/database.js
+++ config/database.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log(`Connected to Database: ${conn.connection.name}`);
--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
 
 module.exports = function (passport) {
+  // Login Strategy
   passport.use(
     new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
       try {
@@ -9,22 +10,31 @@ module.exports = function (passport) {
         if (!user) {
           return done(null, false, { msg: `Email ${email} not found.` });
         }
+
         const isMatch = await user.comparePassword(password);
         if (isMatch) {
           return done(null, user);
+        } else {
+          return done(null, false, { msg: "Invalid email or password." });
         }
-        return done(null, false, { msg: "Invalid email or password." });
       } catch (err) {
         return done(err);
       }
     })
   );
 
+  // Serialize user ID into session
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  // Deserialize user from session
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err);
+    }
   });
 };
